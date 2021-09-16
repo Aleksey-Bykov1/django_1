003f44140ae13d8d80d5f4a4f015ca9bed2c3b0d
Alexey <max_89@inbox.ru>
2021-09-16 08:58:17 +0300

    lesson_6_final

--- baskets/views.py
+++ baskets/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponseRedirect
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+
 from products.models import Product
 from baskets.models import Basket
 
@@ -27,4 +30,15 @@ def basket_remove(request, id):
 
 @login_required()
 def baskets_edit(request, id, quantity):
-    pass
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+
+        baskets = Basket.objects.filter(user=request.user)
+        context = {'baskets': baskets}
+        result = render_to_string('baskets/basket.html', context)
+        return JsonResponse({'result': result})
