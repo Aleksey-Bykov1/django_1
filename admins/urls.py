a8e6262c75b6a8d970f13b9ef0f2c8bd7f6e62d0
Alexey <max_89@inbox.ru>
2021-09-17 14:49:36 +0300

    lesson_7_add_admins

--- admins/urls.py
+++ admins/urls.py
@@ -14,20 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from baskets.views import basket_add, basket_remove, baskets_edit
 
-from products.views import index
+app_name = 'baskets'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls', namespace='products')),
-    path('users/', include('users.urls', namespace='users')),
-    path('baskets/', include('baskets.urls', namespace='baskets')),
-    path('admins/', include('admins.urls', namespace='admins'))
-]
+    path('basket_add/<int:id>', basket_add, name='basket_add'),
+    path('basket_remove/<int:id>', basket_remove, name='basket_remove'),
+    path('edit/<int:id>/<int:quantity>/', baskets_edit, name='baskets_edit'),
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
