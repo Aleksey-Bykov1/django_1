681957115abe975fb69fe371c9218b2b058ab6dc
Alexey <max_89@inbox.ru>
2021-09-28 15:24:01 +0300

    part_2_lesson_1_final_2

--- users/models.py
+++ users/models.py
@@ -11,7 +11,7 @@ from django.utils.timezone import now
 class User(AbstractUser):
     image = models.ImageField(upload_to='user_image', blank=True)
     age = models.PositiveIntegerField(verbose_name='возраст', default=18, blank=True, null=True)
-    activation_key = models.CharField(max_length=128, blank=True)
+    activation_key = models.CharField(max_length=128, blank=True, default=18)
     activation_key_expires = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def is_activation_key_expires(self):
