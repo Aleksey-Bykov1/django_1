57bd53fc7d6b4e2611131ea6c4f27b292c7fe553
Alexey <max_89@inbox.ru>
2021-09-30 12:57:37 +0300

    part_2_lesson_2_add_UsPr

--- users/models.py
+++ users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 
 
@@ -19,3 +21,21 @@ class User(AbstractUser):
             return False
         return True
 
+
+class UserProfile(models.Model):
+
+    MALE = 'М'
+    FEMALE = 'Ж'
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж')
+    )
+
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    tagline = models.CharField(verbose_name='Тэги', max_length=128, blank=True)
+    about_me = models.TextField(verbose_name='О себе', blank=True)
+    gender = models.CharField(verbose_name='Пол', choices=GENDER_CHOICES, blank=True, max_length=5)
+
+    @receiver(post_save,sender=User)
+    def save_user_profile(sender,instance,**kwargs):
+        instance.userprofile.save()
