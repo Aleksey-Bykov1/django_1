1df0867504ee98ac991181069c3f3224c88dcfe2
Alexey <max_89@inbox.ru>
2021-10-07 09:06:48 +0300

    part_2_lesson_3_add_pagin

--- products/views.py
+++ products/views.py
@@ -18,10 +18,11 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-class ProductListView(ListView, Paginator):
+class ProductListView(ListView):
     model = Product
     template_name = 'products/products.html'
     context_object_name = 'products'
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductListView, self).get_context_data(**kwargs)
