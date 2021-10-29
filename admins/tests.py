d61536e24a42de10cc6663622c1efddd03aaef9a
Alexey <max_89@inbox.ru>
2021-10-29 14:42:01 +0300

    part_2_lesson_8_finish

--- admins/tests.py
+++ admins/tests.py
@@ -11,15 +11,6 @@ class TestMainSmokeTest(TestCase):
     email = 'django@mail.ru'
     password = 'geekshop'
 
-    new_user_data = {
-        'username': 'test_django',
-        'first_name': 'Django',
-        'last_name': 'Django2',
-        'password1': 'DjAnG0',
-        'password2': 'DjAnG0',
-        'email': 'dj_super@bk.ru'
-    }
-
     def setUp(self) -> None:
         self.user = User.objects.create_superuser(self.username, email=self.email, password=self.password)
         self.client = Client()
@@ -27,38 +18,9 @@ class TestMainSmokeTest(TestCase):
     def test_admin_users_read(self):
         response = self.client.get('/admins/users/')
         self.client.login(username=self.username, password=self.password)
-        self.assertEqual(response.status_code, self.status_code_success)
+        self.assertEqual(response.status_code, self.status_code_render)
 
     def test_admin_user_create(self):
         response = self.client.get('/admins/category/')
         self.client.login(username=self.username, password=self.password)
-        self.assertEqual(response.status_code, self.status_code_success)
-
-    def test_admin_user_update(self):
-        pass
-
-    def admin_category_read(self):
-        pass
-
-    def admin_category_update(self):
-        pass
-
-    def admin_product_read(self):
-        pass
-
-    # def test_login(self):
-    #     response = self.client.get('/')
-    #     self.assertEqual(response.status_code, self.status_code_success)
-    #     self.assertTrue(response.context['user'].is_anonymous)
-    #
-    #     self.client.login(username=self.username, password=self.password)
-    #     response = self.client.get('/users/login/')
-    #     self.assertEqual(response.status_code, self.status_code_render)
-    #
-    # def test_register(self):
-    #     response = self.client.post('/users/register/', data=self.new_user_data)
-    #     self.assertEqual(response.status_code, self.status_code_render)
-    #     new_user = User.objects.get(username=self.new_user_data['username'])
-
-
-
+        self.assertEqual(response.status_code, self.status_code_render)
